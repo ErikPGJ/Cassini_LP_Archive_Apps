43bca45471f504672a706e959d3172a432be5812
Erik P G Johansson <erik.johansson@irfu.se>
2018-07-02 17:17:33 +0200

    Update (non-used) keep_incrementing_values

--- cnt_cur/keep_incrementing_values.m
+++ cnt_cur/keep_incrementing_values.m
@@ -1,16 +1,22 @@
 %
-% Function for removing array values to create an array of monotonically increasing values.
+% Function for removing array values to make it easy to create an array of monotonically increasing values.
 %
-% This function is mostly intended to replace code in Read_Density.m. Note that that the old implementation (1) only
-% worked on bias timestamps (t_DAC), and not measured sample timestamps (t), and (2) removed timestamps that decreased
-% compared to the immediately preceeding timestamp, i.e. did not guarantee a non-monotonic time series in the end.
+% This function is mostly intended to (maybe) replace code in Read_Density.m.
+% The (old) implementation of Read_density.m that does not use this function
+% (1) removes timestamps that decrease compared to the immediately preceeding timestamp, i.e. do not guarantee a
+% non-monotonic time series in the end, and
+% (2) sets non-incrementing MEASURED values to NaN (kees the indices),
+% but removes non-incrementing DAC values (removes the indices).
+% This function's return result can be used to achieve both types of handling.
+%
+% NOTE: As of 2018-07-02, Read_Density.m does not use this function.
 %
 %
 % ARGUMENTS AND RETURN VALUES
 % ============================
-% tArray : 1D array of scalars.
-% iKeep  : Indices into tArray that point to values which are (strictly) higher than all previous values.
-%          Empty array is represented by size 1x0 array.
+% tArray : 1D array of logical.
+% iKeep  : Logical array same size as tArray. True for indices where tArray values are (strictly) higher than all previous values.
+%          Empty tArray is represented by size 1x0 array.
 %
 %
 % Created 2018-05-14 by Erik P G Johansson.
@@ -21,26 +27,28 @@ function iKeep = keep_incrementing_values(tArray)
     %if ~isvector(tArray)    % NOTE: isvector([]) == false; isvector(zeros(1,0)) == true
     %    error('Argument is not a vector.')
     %end
-    
+
     % ASSERTION
     if any(~isfinite(tArray))
         error('Argument contains non-finite value(s).')
     end
 
-    
-    
-    if issorted(tArray)
-        iKeep = 1:numel(tArray);   % Works for tArray = [];
-    else
+
+
+    iKeep = true(size(tArray));
+    if ~issorted(tArray)
         % CASE: tArray is not sorted.
         % CASE: numel(tArray) >= 1      NOTE: issorted([]) returns true.
-        iKeep = 1;
         
+        jLastIncrementing = 1;
         for i = 2:numel(tArray)
-            if tArray(iKeep(end)) < tArray(i)
-                iKeep(end+1) = i;
+            if tArray(jLastIncrementing) < tArray(i)
+                jLastIncrementing = i;
+                % iKeep(i) = true;   % Already set
+            else
+                iKeep(i) = false;
             end
         end
     end
-
+    
 end
--- cnt_cur/keep_incrementing_values___ATEST.m
+++ cnt_cur/keep_incrementing_values___ATEST.m
@@ -8,19 +8,24 @@ function keep_incrementing_values___ATEST
     resultsList = {};
 
     argsList{end+1}    = {[1,2,3,4,5,6,5,6,7, 8, 9]};
-    resultsList{end+1} = {[1,2,3,4,5,6,    9,10,11]};
+    %resultsList{end+1} = {[1,2,3,4,5,6,    9,10,11]};
+    resultsList{end+1} = {logical([1,1,1,1,1,1,0,0,1, 1, 1])};
 
-    argsList{end+1}    = {[-3,-4,-3,-2, 0,10,-1,-2,10,11,12]};
-    resultsList{end+1} = {[ 1,       4, 5, 6,         10,11]};
+    argsList{end+1}    = {        [-3,-4,-3,-2, 0,10,-1,-2,10,11,12]};
+    %resultsList{end+1} = {[ 1,       4, 5, 6,         10,11]};
+    resultsList{end+1} = {logical([ 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1])};
 
     argsList{end+1}    = {[]};
-    resultsList{end+1} = {zeros(1,0)};
+    %resultsList{end+1} = {zeros(1,0)};
+    resultsList{end+1} = {logical([])};
 
     argsList{end+1}    = {[-3]};
-    resultsList{end+1} = {[ 1]};
+    %resultsList{end+1} = {[ 1]};
+    resultsList{end+1} = {logical([1])};
 
-    argsList{end+1}    = {[-3,-1, 1,10,100]};
-    resultsList{end+1} = {[ 1, 2, 3, 4,  5]};
+    argsList{end+1}    = {        [-3,-1, 1,10,100]};
+    %resultsList{end+1} = {[ 1, 2, 3, 4,  5]};
+    resultsList{end+1} = {logical([ 1, 1, 1, 1,  1])};
 
     %argsList{end+1}    = {};
     %resultsList{end+1} = {};
@@ -40,6 +45,7 @@ function keep_incrementing_values___ATEST
         exp = resultsList{i}{1};
 
         if ~isequal(res, exp)
+            i
             res
             exp
             error('FAIL')
